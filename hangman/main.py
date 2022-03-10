1c27c1dca2ee0f4efbe2199c708119b9095d19b2
Da-S0DAP0P <101362476+Da-S0DAP0P@users.noreply.github.com>
2022-03-10 14:35:30 -0600

    Add files via upload

---
+++ hangman/main.py
@@ -0,0 +1,80 @@
+import re
+
+# Get answer
+answer = "What's Up, Doc?"
+
+answer = answer.upper()
+
+answer_guessed = []
+
+# Determine whether characters in answer need to be guessed.
+for answer_character in answer:
+  if re.search("^[A-Z]$",answer_character):
+    answer_guessed.append(False)
+  else:
+    answer_guessed.append(True)
+
+# Game Logic
+guessed_letters = []
+TOTAL_NUM_OF_INCORRECT_GUESSES_ALLOWED = 5
+num_of_incorrect_guesses = 0
+
+while num_of_incorrect_guesses < TOTAL_NUM_OF_INCORRECT_GUESSES_ALLOWED and False in answer_guessed:
+  print("---------------")
+  print("Letters Guessed: ", end = "")
+  
+  for current_guessed_letters in guessed_letters:
+    print(f"{current_guessed_letters}", end = "")
+
+  print()
+
+  print(f"Number of Incorrect Guesses Remaining: {TOTAL_NUM_OF_INCORRECT_GUESSES_ALLOWED - num_of_incorrect_guesses}")
+
+  print()
+
+  for answer_index in range(len(answer)):
+    if answer_guessed[answer_index]:
+      print(answer[answer_index], end = "")
+    else:
+      print("_", end = "")
+
+  print()
+  print()
+
+  letter = input("Enter A Letter: ")
+
+  letter = letter.upper()
+
+  if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$",letter):
+    #Process the letter in the puzzle.
+    guessed_letters_insert_index = 0
+
+    for current_guessed_letters in guessed_letters:
+      if letter < current_guessed_letters:
+        break
+
+      guessed_letters_insert_index += 1
+
+    guessed_letters.insert(guessed_letters_insert_index, letter)
+    
+    if letter in answer:
+      #Letter is in puzzle.
+      for answer_index in  range(len(answer)):
+        if letter == answer[answer_index]:
+          answer_guessed[answer_index] = True
+    else:
+      #Letter is not in the puzzle.
+      num_of_incorrect_guesses += 1
+
+#Post-Game Summary
+print()
+      
+if num_of_incorrect_guesses < TOTAL_NUM_OF_INCORRECT_GUESSES_ALLOWED:
+  print("YOU WIN!!!  You Solved The Puzzle!!")
+else:
+  print("Sorry, you lose.  You ran out of guesses.")
+
+print(f"{answer} is the answer to the puzzle.")
+
+print()
+      
